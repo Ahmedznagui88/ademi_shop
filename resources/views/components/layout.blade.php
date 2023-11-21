69d4350020c2de634d8d4f1d5cd6f48f8bd9b2f0
“Ivan <ivanschena@gmail.com>
2023-11-21 19:16:12 +0100

    start progetto

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite (['resources/css/app.css', 'resources/js/app.js'])
+    <title>Ademi</title>
+    
+</head>
+<body>
+    <x-nav/>
+    <div class="min-vh-100">
+        
+        {{$slot}}
+    </div>
+</body>
+</html>
