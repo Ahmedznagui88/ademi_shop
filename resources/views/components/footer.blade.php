308ea1a46140fb9ad965fc840cd06ce941e8c2bc
Damiano <arduini.damiano@gmail.com>
2023-11-22 20:20:26 +0100

    index

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,1 @@
-<div class="card" style="width: 18rem;">
-    <img src="{{ Storage::url($ad->image) }}" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Titolo: {{ $ad->title }}</h5>
-        <p>Marchio: {{ $ad->brand }}</p>
-        <p class="card-text">Descrizione: {{ $ad->description }}</p>
-        <p>{{ $ad->price }}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-</div>
+
