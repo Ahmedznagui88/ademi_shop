545c67cb64aba6ec92e20ad549de81b7e681e849
Damiano <arduini.damiano@gmail.com>
2023-11-22 23:12:33 +0100

    update

--- app/Livewire/AdCreateForm.php
+++ app/Livewire/AdCreateForm.php
@@ -21,7 +21,7 @@ class AdCreateForm extends Component
         'title' => 'required|min:3|max:100',
         'brand' => 'required|min:3|max:100',
         'description' => 'required|min:10|max:10000',
-        'image' => 'required|image|mimes:webp,png,jpeg,jpg',
+        
 
     ];
 
@@ -30,20 +30,28 @@ class AdCreateForm extends Component
         'required' => 'Il campo deve essere compilato',
         'min' => 'Il campo deve contenere minimo :min caratteri',
         'max' => 'Il campo deve contenere massimo :max caratteri',
-        'image' => 'Il file deve essere un\'immagine',
+       
         'mimes' => 'Le estensioni devono essere :values',
 
 
     ];
 
+
+
     public function store()
-    {
-        $this->validate();
+    {  
+         $this->validate();
+
+      if($this->image){
+        $image = $this->image->store('public/ads');
+      } else {
+        $image = 'public/media/default-img.jpg';
+      }
         ad::create([
             'title' => $this->title,
             'brand' => $this->brand,
             'description' => $this->description,
-            'image' => $this->image->store('public/ads'),
+            'image' => $image,
             'price' => $this->price,
         ]);
 
--- database/migrations/2023_11_22_194558_add_user_id_column_to_ads_table.php
+++ database/migrations/2023_11_22_194558_add_user_id_column_to_ads_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('ads', function (Blueprint $table) {
 
-            $table->unsignedBigInteger('user_id')->after('id')->nullable;
+            $table->unsignedBigInteger('user_id')->after('id')->nullable();
  
             $table->foreign('user_id')->references('id')->on('users');
         });
