0ac50ecffa895f40a44982d347cc55202166d4e4
Ahmed Znagui <amed.bob88@gmail.com>
2023-11-22 23:36:12 +0100

    card

--- app/Models/ad.php
+++ app/Models/ad.php
@@ -19,7 +19,7 @@ class Ad extends Model
         
         ];
 
-public function user(){
-    return $this->belongsTo(User::class);
-}
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,13 +1,16 @@
-<div class="card rounded-0" style="width: 18rem;">
-    <img src="{{ Storage::url($ad->image) }}" class="card-img-top" alt="{{ $ad->title }}">
-    <div class="card-body text-center">
-        <h5 class="card-title">{{ $ad->title }}</h5>
-        <p>{{ $ad->brand }}</p>
-        <p class="card-text">{{ $ad->description }}</p>
-        <p class="fs-2 price">${{ $ad->price }}</p>
-        <div>
-            <a href="{{ route('ad.show', $ad) }}" class="btn btn-primary w-75">Mostra</a>
-            <i class="fa-regular fa-heart fa-xl mx-3 "></i>
+<div class="out d-flex justify-content-end">
+
+    <div class="card rounded-0" style="width: 18rem;">
+        <img src="{{ Storage::url($ad->image) }}" class="card-img-top" alt="{{ $ad->title }}">
+        <div class="card-body text-center">
+            <h5 class="card-title">{{ $ad->title }}</h5>
+            <p>{{ $ad->brand }}</p>
+            <p class="card-text">{{ $ad->description }}</p>
+            <p class="fs-2 price">${{ $ad->price }}</p>
+            <div class=" d-flex align-items-center justify-content-between">
+                <a href="{{ route('ad.show', $ad) }}" class="btn btn-primary w-50">Mostra</a>
+                <i class="fa-regular fa-heart fa-xl mx-5 "></i>
+            </div>
         </div>
     </div>
 </div>
