e8d7893e1fc785b121f84fe1cae452d62596cff5
“Ivan <ivanschena@gmail.com>
2023-11-23 21:14:59 +0100

    Soluzione Categorie

--- resources/views/Ad/indexCategory.blade.php
+++ resources/views/Ad/indexCategory.blade.php
@@ -2,8 +2,8 @@
 
     <div class="container">
         <div class="row">
-      @forelse ($category->ads as $category )
-          <x-card :category="$category" />
+      @forelse ($category->ads as $ad)
+          <x-card :ad="$ad"/>
       @empty
           <h2>Noni ci sono categorie per questo prodotto <a href="{{ route('ad.create') }}"> aggiungine una</a></h2>
       @endforelse
