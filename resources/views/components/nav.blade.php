9c4a4380acfc1f142d383c8a09caa8deeb03fab3
Damiano <arduini.damiano@gmail.com>
2023-11-25 15:57:38 +0100

    update

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg ">
+  <nav class="navbar navbar-expand-lg ">
     <div class="container-fluid">
         <a href="{{ route('homepage') }}" class="">
             <img src="/media/ademi logo 2.png" class="logo" alt="">
@@ -13,9 +13,9 @@
                     <li class="nav-item ">
                         <a class="nav-link " aria-current="page" href="">{{ Auth::user()->name }}</a>
                     </li>
-                    {{-- <li class="nav-item">
+                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="{{ route('homepage') }}">Home</a>
-                    </li> --}}
+                    </li> 
                     <li class="nav-item">
                         <a class="nav-link" href="{{ Route('ad.create') }}">Inserisci annuncio</a>
                     </li>
@@ -39,41 +39,6 @@
             </ul>
         </div>
     </div>
-</nav>
+</nav> 
 
 
-
-{{-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a href="{{ route('homepage') }}" class="">
-            <img src="/media/logo.png" class="logo" alt="">
-        </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-         
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown link
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-</nav> --}}
