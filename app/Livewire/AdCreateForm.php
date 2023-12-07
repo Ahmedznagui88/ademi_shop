19c7a3353194afbe5160bec2dce6146606bd4998
“Ivan <ivanschena@gmail.com>
2023-12-07 20:57:47 +0100

    gCredentialOk

--- app/Livewire/AdCreateForm.php
+++ app/Livewire/AdCreateForm.php
@@ -2,7 +2,7 @@
 
 namespace App\Livewire;
 
-
+use App\Jobs\GoogleVisionSafeSearch;
 use Livewire\Component;
 use App\Models\Category;
 use App\Jobs\ResizeImage;
@@ -89,6 +89,7 @@ class AdCreateForm extends Component
                 $newFileName = "ads/{$this->ad->id}";
                 $newImage = $this->ad->images()->create(['path' => $image->store($newFileName, 'public')]);
                 dispatch(new ResizeImage($newImage->path, 300, 300));
+                dispatch(new GoogleVisionSafeSearch($newImage->id));
             }
 
             File::deleteDirectory(storage_path('app/livewire-tmp'));
