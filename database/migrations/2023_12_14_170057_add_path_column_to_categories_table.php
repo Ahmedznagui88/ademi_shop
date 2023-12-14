21eb22cda3af2707ed23ae9df094c20baa5ba36c
“Ivan <ivanschena@gmail.com>
2023-12-14 19:04:16 +0100

    SeedamiBannersThanksToEma

--- database/migrations/2023_12_14_170057_add_path_column_to_categories_table.php
+++ database/migrations/2023_12_14_170057_add_path_column_to_categories_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->string('path')->nullable()->after('name');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->dropColumn('path');
         });
     }
 };
