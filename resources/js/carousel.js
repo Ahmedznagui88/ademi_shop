3ea1905cfd44ce4723d0ed8d7b3523368369c544
Ahmed Znagui <amed.bob88@gmail.com>
2023-12-16 21:01:55 +0100

    up

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -2,68 +2,63 @@ gsap.registerPlugin(ScrollTrigger);
 
 //!txt carousel
 gsap.to(".testo-h5",{
-    y: -50,
-      duration: 20,
+    y: -55,
       rotateX: -5,
       scrollTrigger: {
           opacity: 1,
           trigger: ".testo-h5",
           start: "top 110%",
           end: "bottom 10%",
-          scrub: 1,
+/*           scrub: 1, */
       },
   }) 
 
   //*section animation
-/*   gsap.to(".divSecImg",{
-    y: -50,
-      duration: 20,
+gsap.to(".divSecImg",{
+    y: -80,
       rotateX: -5,
       scrollTrigger: {
           opacity: 1,
           trigger: ".divSecImg",
           start: "top 110%",
           end: "bottom 10%",
-          scrub: 1,
+/*           scrub: 1, */
       },
   })  
   
   
   gsap.to(".divSecImg2",{
-    y: -50,
-      duration: 20,
+    y: -80,
       rotateX: -5,
       scrollTrigger: {
           opacity: 1,
           trigger: ".divSecImg2",
           start: "top 110%",
           end: "bottom 10%",
-          scrub: 1,
+/*           scrub: 1, */
       },
   }) 
 
   gsap.to(".divSecImg3",{
-    y: -50,
-      duration: 20,
+    y: -80,
       rotateX: -5,
       scrollTrigger: {
           opacity: 1,
           trigger: ".divSecImg3",
           start: "top 110%",
           end: "bottom 10%",
-          scrub: 1,
+/*           scrub: 1, */
       },
   }) 
 
   gsap.to(".divSecImg4",{
-    y: -50,
-      duration: 20,
+    y: -80,
       rotateX: -5,
       scrollTrigger: {
           opacity: 1,
           trigger: ".divSecImg4",
           start: "top 110%",
           end: "bottom 10%",
-          scrub: 1,
+/*           scrub: 1, */
       },
-  })  */
+  }) 
